405c0a77b61d4eb0bb019f485f1ab25c039afd07
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-02 22:53:50 -0700

    Create numbers.swift

---
+++ numbers.swift
@@ -0,0 +1,9 @@
+var a = 3
+var b = a + 1
+print(b)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for number in numbers {
+    print("a + \(number)")
+} //a is supposed to be printed as a variable in this instance
