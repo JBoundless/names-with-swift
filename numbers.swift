c6203441925d84647c156e3643c9fdd32a618a6d
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-02 22:59:00 -0700

    Update numbers.swift

--- numbers.swift
+++ numbers.swift
@@ -7,3 +7,6 @@ let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for number in numbers {
     print("a + \(number)")
 } //a is supposed to be printed as a variable in this instance
+for i in 1...1000 {
+    print(i)
+}
