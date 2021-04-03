be487f6749ed31e32a21d6e2f159b9e7173f0474
JBoundless <39219944+JBoundless@users.noreply.github.com>
2021-04-02 22:46:57 -0700

    Create names.swift

---
+++ names.swift
@@ -0,0 +1,8 @@
+let name = "Jason"
+print("Hello \(name)! Great to see you!")
+
+let name_2 = "Jordan"
+print("\(name), meet \(name_2).")
+
+let name_3 = "Todd"
+print("\(name_3), I would like you to meet \(name) and \(name_2).")
